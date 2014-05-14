fe8babd5d4c756975f7cfedae5c4e572117252ba
awoods226 <awoods226@gmail.com>
2014-05-13 20:12:12 -0500

    progress

---
+++ Vision/Controllers/TrainingCompletedController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Vision.Models;
+using Vision.DataAccessLayer;
+
+namespace Vision.Controllers
+{
+    public class TrainingCompletedController : Controller
+    {
+        private VisionContext db = new VisionContext();
+
+        // GET: /TrainingCompleted/
+        public ActionResult Index()
+        {
+            return View(db.TrainingCompleted.ToList());
+        }
+
+        // GET: /TrainingCompleted/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrainingCompleted trainingcompleted = db.TrainingCompleted.Find(id);
+            if (trainingcompleted == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainingcompleted);
+        }
+
+        // GET: /TrainingCompleted/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /TrainingCompleted/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,UserId,TrainingEventId,DateCompleted,ApprovedByUserId")] TrainingCompleted trainingcompleted)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TrainingCompleted.Add(trainingcompleted);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(trainingcompleted);
+        }
+
+        // GET: /TrainingCompleted/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrainingCompleted trainingcompleted = db.TrainingCompleted.Find(id);
+            if (trainingcompleted == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainingcompleted);
+        }
+
+        // POST: /TrainingCompleted/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,UserId,TrainingEventId,DateCompleted,ApprovedByUserId")] TrainingCompleted trainingcompleted)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trainingcompleted).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(trainingcompleted);
+        }
+
+        // GET: /TrainingCompleted/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrainingCompleted trainingcompleted = db.TrainingCompleted.Find(id);
+            if (trainingcompleted == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainingcompleted);
+        }
+
+        // POST: /TrainingCompleted/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TrainingCompleted trainingcompleted = db.TrainingCompleted.Find(id);
+            db.TrainingCompleted.Remove(trainingcompleted);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Vision/Migrations/Configuration.cs
+++ Vision/Migrations/Configuration.cs
@@ -1,19 +1,34 @@
 namespace Vision.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Vision.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Vision.DataAccessLayer.VisionContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Vision.DataAccessLayer.VisionContext context)
         {
+            var divisions = new List<Division>
+            {
+                new Division{Name = "JTAC"}
+            };
+            divisions.ForEach(s => context.Divisions.AddOrUpdate(s));
+            context.SaveChanges();
+            var trainingTables = new List<TrainingTable>
+            {
+                new TrainingTable{Name = "IQT", DivisionId=1} 
+            };
+            trainingTables.ForEach(s => context.TrainingTables.AddOrUpdate(s));
+            context.SaveChanges();
+            
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
